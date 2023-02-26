97ea465d1e2dd493ab58aa7b729529f46a624c3d
g00gol <jackeyyang.work@gmail.com>
2023-02-26 03:21:32 -0500

    minor updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,17 +12,24 @@ ws.onopen = () => {
 
 function App() {
   const [WSData, setWSData] = useState("");
+  const [prevWSData, setPrevWSData] = useState("");
   const [style, setStyle] = useState("reflection");
 
   ws.onmessage = (e) => {
     let res = e.data;
+    setPrevWSData(WSData);
     setWSData(res);
   };
 
   return (
     <>
       <Landing />
-      <InitialScene style={style} data={WSData.split(" ")} scale={20} />
+      <InitialScene
+        style={style}
+        data={WSData.split(" ")}
+        prevData={prevWSData.split(" ")}
+        scale={20}
+      />
     </>
   );
 }
