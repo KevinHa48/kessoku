2c53782709266aee8b04c59d3c9ee2526000da9a
g00gol <jackeyyang.work@gmail.com>
2023-02-25 22:26:17 -0500

    first scene set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <a-scene>
+        <a-marker preset="hiro">
+          <a-box position="0 0 0" material="color: red;"></a-box>
+        </a-marker>
+        <a-entity camera="userHeight: 0;"></a-entity>
+      </a-scene>
+    </>
   );
 }
 
